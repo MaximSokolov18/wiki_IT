67ad12748f64358a52a2e3fde8285fd6d2073677
MaximSokolov18 <makssokolov107@gmail.com>
2023-05-02 22:21:43 +0300

    add definition creation page

--- src/app/pages/definition-creation-page/definition-creation-page.component.ts
+++ src/app/pages/definition-creation-page/definition-creation-page.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+type ArticleData = {
+  a_name: string,
+  topic: string,
+  keywords: Array<string>,
+  content: string,
+  link: string,
+}
 
 @Component({
   selector: 'app-definition-creation-page',
@@ -6,5 +15,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./definition-creation-page.component.scss']
 })
 export class DefinitionCreationPageComponent {
+  articleFields: ArticleData = {
+    a_name: '',
+    topic: '',
+    keywords: [''],
+    content: '',
+    link: ''
+  }
 
+  constructor(private http: HttpClient) { }
+
+  createArticle() {
+    return this.http.post('http://localhost:3000/articles', this.articleFields).subscribe(response => {
+      console.log(response);
+    });
+  }
+
+  changeField(nameField: keyof ArticleData, event: Event) {
+    const value = (event.target as HTMLInputElement)?.value;
+    if (nameField === 'keywords') {
+      this.articleFields[nameField] = value.trim().split(/,+\s+|,/g);
+    } else {
+      this.articleFields[nameField] = value;
+    }
+  }
 }
