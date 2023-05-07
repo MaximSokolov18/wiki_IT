40cbacdda9f1bc4991f0fa9254d3263ddc9b42c9
MaximSokolov18 <makssokolov107@gmail.com>
2023-05-07 22:23:43 +0300

    add menu for main page

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MatTreeNestedDataSource} from "@angular/material/tree";
+import {NestedTreeControl} from "@angular/cdk/tree";
+
+type Node = {
+  name: string;
+  children?: Node[];
+}
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +14,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+  treeControl = new NestedTreeControl<Node>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<Node>();
+  articles: any = [];
+  hasChild = (_: number, node: Node) => !!node.children && node.children.length > 0;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getArticles();
+  }
+
+  getArticles() {
+    this.http.get('http://localhost:3000/articles').subscribe((response) => {
+      this.articles = Object.entries((response as Array<any>).reduce((acc, item) => {
+        return {
+          ...acc,
+          [item.id_topic]: acc?.[item.id_topic] ? [...acc?.[item.id_topic], item] : [item]
+        }
+      }, {})).map(([name, children]) => ({name, children: (children as Array<any>).map((item) => ({name: item.a_name}))}));
+
+      this.dataSource.data = this.articles;
+    });
+  }
 }
