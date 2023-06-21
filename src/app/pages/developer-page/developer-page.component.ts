99bd6df768cef975fa5390f589714c2d22ad2e89
Max Komar <silencecut@gmail.com>
2023-06-21 20:08:42 +0300

    add more info about developer

--- src/app/pages/developer-page/developer-page.component.ts
+++ src/app/pages/developer-page/developer-page.component.ts
@@ -8,15 +8,18 @@ import { Component } from '@angular/core';
 export class DeveloperPageComponent {
   people = [
     { name: 'Задорожний Олександр Васильович', tasks: '1) Створив базу даних для збереження статтей. '
-    + '2) Розробив бекендну частину, а саме ендпоїнти ',
+    + '2) Розробив бекендну частину, а саме ендпоїнти.',
       url: 'assets/komar/dev-img/Thador.jpg', alt: 'image 1' },
-    { name: 'Комар Максим Леонідович', tasks: 'Вкладка розробників сайту', url: 'assets/komar/dev-img/photo_Komar.png', alt: 'image 2' },
+    { name: 'Комар Максим Леонідович', tasks: 'Створив вкладку розробників сайту', url: 'assets/komar/dev-img/photo_Komar.png', alt: 'image 2' },
     { name: 'Кривко Даніїл Олександрович', tasks: 'Захостив базу даних на https://aws.amazon.com/', url: 'assets/komar/dev-img/photo_Krivko.jpg', alt: 'image 3' },
     { name: 'Рябчич Віталій Володимирович', tasks: '', url: 'assets/komar/dev-img/photo_Ryabchich.jpg', alt: 'image 4' },
     { name: 'Панько Анастасія Павлівна', tasks: 'Додала на сторінці створення окрему форму по '
-    + 'видаленню артікла.', url: 'assets/komar/dev-img/photo_Panko.jpg', alt: 'image 5' },
-    { name: 'Пустовіт Андрій Володимирович', tasks: '', url: 'assets/komar/dev-img/photo_Pustovit.jpg', alt: 'image 6' },
-    { name: 'Соколов Максим Сергійович', tasks: '', url: 'assets/komar/dev-img/photo_Sokolov.jpg', alt: 'image 7' },
-    { name: 'Строганов Янош Романович', tasks: '', url: 'path/to/image1.jpg', alt: 'image 8' },
+    + 'видаленню артиклів.', url: 'assets/komar/dev-img/photo_Panko.jpg', alt: 'image 5' },
+    { name: 'Пустовіт Андрій Володимирович', tasks: 'Створив технічне завдання проекта', url: 'assets/komar/dev-img/photo_Pustovit.jpg', alt: 'image 6' },
+    { name: 'Соколов Максим Сергійович', tasks: 'Розробив сайт IT вікіпедію. На сайті знаходиться '
+    +'меню з трьома сторінками:головна,пошук,створення артиклів.', url: 'assets/komar/dev-img/photo_Sokolov.jpg', alt: 'image 7' },
+    { name: 'Строганов Янош Романович', tasks: 'Змінив paddings для артікла. Змінив '
+    + 'відступи з ліва та справа в артиклі для назви.' 
+    + 'контенту. Можна зробити відступи динамічними.', url: 'path/to/image1.jpg', alt: 'image 8' },
   ];
 }
